b7dc7d595d2575e7a39055ddd5608cf10ad7eee7
Mai Vang <vmaineng@gmail.com>
2022-04-26 17:17:58 -0700

    commit tree 7df05d0a53029ae815c56bbd1be02c46d27c6373

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function () {
+    return (2000/100);
+}
+
+const square = (parameter1) => parameter1 * parameter1;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
